99254f3811da89f4489e423ca97290b7ed7733f0
akm <akm2000@gmail.com>
2016-02-19 17:04:31 +0900

    Use import instead of reference tag

--- modules/Test.ts
+++ modules/Test.ts
@@ -1,13 +1,13 @@
-/// <reference path="Validation.ts" />
-/// <reference path="LettersOnlyValidator.ts" />
-/// <reference path="ZipCodeValidator.ts" />
+import validation = require('./Validation');
+import zip = require('./ZipCodeValidator');
+import letters = require('./LettersOnlyValidator');
 
 // Some samples to try
 var strings = ['Hello', '98052', '101'];
 // Validators to use
-var validators: { [s: string]: Validation.StringValidator; } = {};
-validators['ZIP code'] = new Validation.ZipCodeValidator();
-validators['Letters only'] = new Validation.LettersOnlyValidator();
+var validators: { [s: string]: validation.StringValidator; } = {};
+validators['ZIP code'] = new zip.ZipCodeValidator();
+validators['Letters only'] = new letters.LettersOnlyValidator();
 // Show whether each string passed each validator
 strings.forEach(s => {
     for (var name in validators) {
