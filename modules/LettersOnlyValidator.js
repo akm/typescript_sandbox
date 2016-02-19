cdea9aaffe2eddcdc87ab0a34c12c06decc358db
akm <akm2000@gmail.com>
2016-02-19 17:06:47 +0900

    tsc --module commonjs Test.ts

--- modules/LettersOnlyValidator.js
+++ modules/LettersOnlyValidator.js
@@ -1,14 +1,10 @@
-/// <reference path="Validation.ts" />
-var Validation;
-(function (Validation) {
-    var lettersRegexp = /^[A-Za-z]+$/;
-    var LettersOnlyValidator = (function () {
-        function LettersOnlyValidator() {
-        }
-        LettersOnlyValidator.prototype.isAcceptable = function (s) {
-            return lettersRegexp.test(s);
-        };
-        return LettersOnlyValidator;
-    })();
-    Validation.LettersOnlyValidator = LettersOnlyValidator;
-})(Validation || (Validation = {}));
+var lettersRegexp = /^[A-Za-z]+$/;
+var LettersOnlyValidator = (function () {
+    function LettersOnlyValidator() {
+    }
+    LettersOnlyValidator.prototype.isAcceptable = function (s) {
+        return lettersRegexp.test(s);
+    };
+    return LettersOnlyValidator;
+})();
+exports.LettersOnlyValidator = LettersOnlyValidator;
--- modules/Test.js
+++ modules/Test.js
@@ -1,12 +1,11 @@
-/// <reference path="Validation.ts" />
-/// <reference path="LettersOnlyValidator.ts" />
-/// <reference path="ZipCodeValidator.ts" />
+var zip = require('./ZipCodeValidator');
+var letters = require('./LettersOnlyValidator');
 // Some samples to try
 var strings = ['Hello', '98052', '101'];
 // Validators to use
 var validators = {};
-validators['ZIP code'] = new Validation.ZipCodeValidator();
-validators['Letters only'] = new Validation.LettersOnlyValidator();
+validators['ZIP code'] = new zip.ZipCodeValidator();
+validators['Letters only'] = new letters.LettersOnlyValidator();
 // Show whether each string passed each validator
 strings.forEach(function (s) {
     for (var name in validators) {
