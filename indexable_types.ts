e00a3424cff8837efd94732490a30a80afd8c3b0
akm <akm2000@gmail.com>
2018-10-16 19:05:10 +0900

    :+1: Add some examples

---
+++ indexable_types.ts
@@ -0,0 +1,12 @@
+class Animal {
+    name: string;
+}
+class Dog extends Animal {
+    breed: string;
+}
+
+// Error: indexing with a numeric string might get you a completely separate type of Animal!
+interface NotOkay {
+    [x: number]: Animal;
+    [x: string]: Dog;
+}
