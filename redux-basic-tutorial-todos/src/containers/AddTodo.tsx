be54ec3ac759f20bd935d8ae43b8ef4e6f699bef
akm <akm2000@gmail.com>
2020-05-24 12:59:00 +0900

    :+1: コンパイルできるように型を追加

--- redux-basic-tutorial-todos/src/containers/AddTodo.tsx
+++ redux-basic-tutorial-todos/src/containers/AddTodo.tsx
@@ -1,27 +1,28 @@
 import React from 'react'
 import { connect } from 'react-redux'
 import { addTodo } from '../actions'
+import { AppDispatch } from '../store'
 
-const AddTodo = ({ dispatch }) => {
-  let input
+const AddTodo = ({ dispatch }: { dispatch: AppDispatch }) => {
+    let input: HTMLInputElement
 
-  return (
-    <div>
-      <form onSubmit={e => {
-        e.preventDefault()
-        if (!input.value.trim()) {
-          return
-        }
-        dispatch(addTodo(input.value))
-        input.value = ''
-      }}>
-        <input ref={node => input = node} />
-        <button type="submit">
-          Add Todo
+    return (
+        <div>
+            <form onSubmit={e => {
+                e.preventDefault()
+                if (!input.value.trim()) {
+                    return
+                }
+                dispatch(addTodo(input.value))
+                input.value = ''
+            }}>
+                <input ref={node => input = node as HTMLInputElement} />
+                <button type="submit">
+                    Add Todo
         </button>
-      </form>
-    </div>
-  )
+            </form>
+        </div>
+    )
 }
 
 export default connect()(AddTodo)
