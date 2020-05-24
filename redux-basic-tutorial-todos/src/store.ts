56f2be27e679f0bbf38b757d9e47269a08c294ca
akm <akm2000@gmail.com>
2020-05-24 17:47:55 +0900

    AppにRouterを被せるRootを追加

--- redux-basic-tutorial-todos/src/store.ts
+++ redux-basic-tutorial-todos/src/store.ts
@@ -4,4 +4,5 @@ import rootReducer from './reducers'
 export const store = createStore(rootReducer)
 
 // https://redux.js.org/recipes/usage-with-typescript#typing-configurestore
+export type AppStore = typeof store
 export type AppDispatch = typeof store.dispatch
