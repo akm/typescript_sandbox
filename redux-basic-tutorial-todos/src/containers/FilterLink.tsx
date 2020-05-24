ff514268395d33e7c871d9bfa783015960123745
akm <akm2000@gmail.com>
2020-05-24 21:41:10 +0900

    拡張子をtsxに変更し、閉じタグを修正

--- redux-basic-tutorial-todos/src/containers/FilterLink.tsx
+++ redux-basic-tutorial-todos/src/containers/FilterLink.tsx
@@ -4,15 +4,15 @@ import { NavLink } from 'react-router-dom'
 
 const FilterLink = ({ filter, children }: { filter: VisibilityFilter, children: React.Component | string }) => (
     <NavLink
-    exact
-to = { filter === 'SHOW_ALL' ? '/' : `/${filter}` }
-activeStyle = {{
-    textDecoration: 'none',
-        color: 'black'
-}}
-  >
-    { children }
-    < /NavLink>
+        exact
+        to={filter === 'SHOW_ALL' ? '/' : `/${filter}`}
+        activeStyle={{
+            textDecoration: 'none',
+            color: 'black'
+        }}
+    >
+        {children}
+    </NavLink>
 )
 
 export default FilterLink
