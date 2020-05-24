e7f15f4c4e6dfaa44dcf41ee6e1cbed4e62d7124
akm <akm2000@gmail.com>
2020-05-24 12:12:48 +0900

    :recycle: src/store.ts として store を抽出

--- redux-basic-tutorial-todos/src/index.tsx
+++ redux-basic-tutorial-todos/src/index.tsx
@@ -1,15 +1,12 @@
 import React from 'react'
 import { render } from 'react-dom'
-import { createStore } from 'redux'
 import { Provider } from 'react-redux'
 import App from './components/App'
-import rootReducer from './reducers'
-
-const store = createStore(rootReducer)
+import { store } from './store'
 
 render(
-  <Provider store={store}>
-    <App />
-  </Provider>,
-  document.getElementById('root')
+    <Provider store={store}>
+        <App />
+    </Provider>,
+    document.getElementById('root')
 )
