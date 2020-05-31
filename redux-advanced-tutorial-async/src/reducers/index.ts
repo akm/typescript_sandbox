7df0a1ef27ee97423ead095d323149062fe9453e
akm <akm2000@gmail.com>
2020-05-31 16:41:46 +0900

    :sparkles: reducerを追加

---
+++ redux-advanced-tutorial-async/src/reducers/index.ts
@@ -0,0 +1,50 @@
+import { Reducer } from 'redux';
+import { AxiosError } from 'axios';
+
+import { Actions } from '../actions';
+import * as ActionTypes from '../actions/types';
+import { Post } from '../services/reddit/models';
+
+export interface RedditState {
+    posts: Post[];
+    isLoading: boolean;
+    error?: AxiosError | null;
+}
+export const initialState: RedditState = {
+    posts: [],
+    isLoading: false,
+};
+
+const redditReducer: Reducer<RedditState, Actions> = (
+    state: RedditState = initialState,
+    action: Actions,
+): RedditState => {
+    switch (action.type) {
+        case ActionTypes.REDDIT_GET_POSTS_START:
+            return {
+                ...state,
+                posts: [],
+                isLoading: true
+            }
+        case ActionTypes.REDDIT_GET_POSTS_SUCCEED:
+            return {
+                ...state,
+                posts: action.payload.result.posts,
+                isLoading: false,
+                error: null,
+            }
+        case ActionTypes.REDDIT_GET_POSTS_FAIL:
+            return {
+                ...state,
+                posts: [],
+                isLoading: false,
+                error: action.payload.error,
+            }
+        case ActionTypes.SELECT_SUBREDDIT:
+            return {
+                ...state,
+            }
+    }
+}
+
+export default redditReducer;
