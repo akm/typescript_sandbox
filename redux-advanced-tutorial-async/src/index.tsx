f531b3841e8651e4f0fbb959f5f4e6072cf1b832
akm <akm2000@gmail.com>
2020-05-31 17:02:49 +0900

    :sparkles: reducerとsagaを組み込み

--- redux-advanced-tutorial-async/src/index.tsx
+++ redux-advanced-tutorial-async/src/index.tsx
@@ -1,11 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+import createSagaMiddleware from 'redux-saga';
+
 import App from './components/App';
+import reducer from './reducers';
+import rootSaga from './sagas/reddit';
+
+import './index.css';
+
+const sagaMiddleWare = createSagaMiddleware();
+const store = createStore(reducer, applyMiddleware(sagaMiddleWare));
 
 ReactDOM.render(
     <React.StrictMode>
-        <App />
+        <Provider store={store}>
+            <App />
+        </Provider>
     </React.StrictMode>,
     document.getElementById('root')
 );
+
+sagaMiddleWare.run(rootSaga);
