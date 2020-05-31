46e0757e53145c05bb28364bccfdd96a2c096dba
akm <akm2000@gmail.com>
2020-05-31 13:38:36 +0900

    :sparkles: ダミーのデータを表示するようにApp.tsxを変更

--- redux-advanced-tutorial-async/src/App.tsx
+++ redux-advanced-tutorial-async/src/App.tsx
@@ -1,24 +1,37 @@
 import React from 'react';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div>
+            <span>
+                <h1>reactjs</h1>
+                <select value="reactjs">
+                    <option value="reactjs" key="reactjs">reactjs</option>
+                    <option value="frontend" key="frontend">frontend</option>
+                </select>
+            </span>
+
+            <p>
+                <span>
+                    Last updated at {new Date("2020-05-31 13:32:54").toLocaleTimeString()}.
+                    {' '}
+                </span>
+
+                <button >
+                    Refresh
+                </button>
+            </p>
+
+            <div style={{ opacity: 1 }}>
+                <ul>
+                    <li key="0">foo</li>
+                    <li key="1">bar</li>
+                    <li key="2">baz</li>
+                </ul >
+            </div >
+
+        </div >
+    );
 }
 
 export default App;
