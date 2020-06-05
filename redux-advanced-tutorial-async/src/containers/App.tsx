5308656fc653f234c4728068ae3a0074cd3b5491
akm <akm2000@gmail.com>
2020-06-05 21:09:59 +0900

    :+1: subredditをURLのパスから取得するように変更

--- redux-advanced-tutorial-async/src/containers/App.tsx
+++ redux-advanced-tutorial-async/src/containers/App.tsx
@@ -1,12 +1,13 @@
 import React, { FC, useEffect } from 'react';
 import { connect } from 'react-redux';
 import { bindActionCreators, Dispatch } from 'redux';
+import { RouteComponentProps, withRouter } from 'react-router';
 
 import AppComponent, { AppProps } from '../components/App'
 import { RedditState, initialState } from '../reducers';
 import { getPosts, selectSubreddit } from '../actions';
 
-type StateProps = Pick<AppProps, 'subreddit' | 'lastUpdatedAt' | 'isLoading'>
+type StateProps = Pick<AppProps, 'lastUpdatedAt' | 'isLoading'>
 
 const mapStateToProps = (state: RedditState = initialState): StateProps => state;
 
@@ -23,12 +24,21 @@ const mapDispatchToProps = (dispatch: Dispatch): DispatchProps => bindActionCrea
     dispatch
 );
 
-const App: FC<StateProps & DispatchProps> = ({
+type EnhancedProps =
+    StateProps & DispatchProps &
+    RouteComponentProps<{ subreddit: string }>;
+
+const App: FC<EnhancedProps> = ({
     handleChangeSubreddit,
     getPostsStart,
-    subreddit,
+    history,
+    match,
     ...rest
 }) => {
+    const subreddit = match.params.subreddit
+    console.log("containers/App.tsx subreddit", subreddit)
+    console.log("containers/App.tsx rest", rest)
+
     useEffect(() => {
         getPostsStart(subreddit);
     }, [subreddit]);
@@ -36,11 +46,11 @@ const App: FC<StateProps & DispatchProps> = ({
     const props = {
         subreddit,
         onRefreshClick: () => getPostsStart(subreddit),
-        onChangeSubreddit: handleChangeSubreddit,
+        onChangeSubreddit: (val: string) => history.push("/" + val),
         ...rest
     }
 
     return <AppComponent {...props} />
 }
 
-export default connect(mapStateToProps, mapDispatchToProps)(App)
+export default withRouter(connect(mapStateToProps, mapDispatchToProps)(App))
