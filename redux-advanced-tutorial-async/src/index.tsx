05aa596e18263b87b07006da10c371deffa606dd
akm <akm2000@gmail.com>
2020-06-06 17:20:09 +0900

    :wrench: Redux DevToolsの設定を追加

--- redux-advanced-tutorial-async/src/index.tsx
+++ redux-advanced-tutorial-async/src/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
 import createSagaMiddleware from 'redux-saga';
 import { BrowserRouter } from 'react-router-dom';
 import { createLogger } from 'redux-logger'
@@ -12,9 +12,19 @@ import Root from './Root';
 
 import './index.css';
 
+/* eslint-disable no-underscore-dangle, @typescript-eslint/no-explicit-any */
+const composeEnhancers =
+    process.env.NODE_ENV === 'development' &&
+        typeof window === 'object' &&
+        (window as any).__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
+        ? (window as any).__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({})
+        : compose;
+/* eslint-enable */
+
 const sagaMiddleWare = createSagaMiddleware();
 const middlewares = [sagaMiddleWare, createLogger()]
-const store = createStore(reducer, applyMiddleware(...middlewares));
+const enhancer = composeEnhancers(applyMiddleware(...middlewares));
+const store = createStore(reducer, enhancer);
 
 ReactDOM.render(
     <React.StrictMode>
