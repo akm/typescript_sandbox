dfad8d1a598aede56ca0be213c80cd9c6ea0c1fa
akm <akm2000@gmail.com>
2020-06-07 18:00:14 +0900

    :sparkles: ドキュメント通りにコピーしているつもりだけどassetsタブがうまく動かない・・・

--- learnstorybook.com-intro/taskbox/.storybook/design-addon/register.js
+++ learnstorybook.com-intro/taskbox/.storybook/design-addon/register.js
@@ -1,7 +1,9 @@
+// .storybook/design-addon/register.js
 import React, { Fragment } from 'react';
-import { AddonPanel } from '@storybook/components';
-import { useParameter, useStorybookState } from '@storybook/api';
+
+import { useParameter, useStorybookState, useAddonState } from '@storybook/api';
 import { addons, types } from '@storybook/addons';
+import { AddonPanel, ActionBar } from '@storybook/components';
 import { styled } from '@storybook/theming';
 
 const getUrl = input => {
@@ -25,7 +27,6 @@ const Asset = ({ url }) => {
     return null;
   }
   if (url.match(/\.(png|gif|jpeg|tiff|svg|anpg|webp)/)) {
-    // do image viewer
     return <Img alt="" src={url} />;
   }
 
@@ -33,20 +34,44 @@ const Asset = ({ url }) => {
 };
 
 const Content = () => {
-  // story's parameter being retrieved here
-  const results = useParameter('assets', []);
-  // the id of story retrieved from Storybook global state
-  const { storyId } = useStorybookState();
+  const results = useParameter('assets', []); // story's parameter being retrieved here
+  const [selected, setSelected] = useAddonState('my/design-addon', 0); // addon state being persisted here
+  const { storyId } = useStorybookState(); // the story«s unique identifier being retrieved from Storybook global state
 
   if (results.length === 0) {
     return null;
   }
 
-  const url = getUrl(results[0]).replace('{id}', storyId);
+  if (results.length && !results[selected]) {
+    setSelected(0);
+    return null;
+  }
+
+  const url = getUrl(results[selected]).replace('{id}', storyId);
 
   return (
     <Fragment>
       <Asset url={url} />
+      {results.length > 1 ? (
+        <ActionBar
+          actionItems={results.map((i, index) => ({
+            title: typeof i === 'string' ? `asset #${index + 1}` : i.name,
+            onClick: () => setSelected(index),
+          }))}
+        />
+      ) : null}
     </Fragment>
   );
 };
+
+addons.register('my/design-addon', () => {
+  addons.add('design-addon/panel', {
+    title: 'assets',
+    type: types.PANEL,
+    render: ({ active, key }) => (
+      <AddonPanel active={active} key={key}>
+        <Content />
+      </AddonPanel>
+    ),
+  });
+});
