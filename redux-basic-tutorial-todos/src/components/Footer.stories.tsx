2ed5c3c1a0a925e00b1ba80fcdde020bce9370ce
akm <akm2000@gmail.com>
2020-06-21 15:13:45 +0900

    :green_book: Footerにa11yのaddonの設定を追加

--- redux-basic-tutorial-todos/src/components/Footer.stories.tsx
+++ redux-basic-tutorial-todos/src/components/Footer.stories.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { MemoryRouter } from 'react-router'
 import { storiesOf } from '@storybook/react';
+import { withA11y } from '@storybook/addon-a11y';
 
 import Footer from './Footer'
 
@@ -8,4 +9,5 @@ storiesOf('Footer', module)
     .addDecorator(story => (
         <MemoryRouter initialEntries={['/']}>{story()}</MemoryRouter>
     ))
+    .addDecorator(withA11y)
     .add('default', () => <Footer />)
