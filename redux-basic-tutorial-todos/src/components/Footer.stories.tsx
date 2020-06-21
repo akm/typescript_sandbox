4b32264fee0df69760f28924f4fdc6356bf22949
akm <akm2000@gmail.com>
2020-06-21 15:14:15 +0900

    :green_book: backgrounds addonの設定を追加

--- redux-basic-tutorial-todos/src/components/Footer.stories.tsx
+++ redux-basic-tutorial-todos/src/components/Footer.stories.tsx
@@ -1,9 +1,16 @@
 import React from 'react';
 import { MemoryRouter } from 'react-router'
-import { storiesOf } from '@storybook/react';
+import { storiesOf, addParameters } from '@storybook/react';
 import { withA11y } from '@storybook/addon-a11y';
 
 import Footer from './Footer'
+
+addParameters({
+    backgrounds: [
+        { name: 'twitter', value: '#00aced', default: true },
+        { name: 'facebook', value: '#3b5998' },
+    ],
+});
 
 storiesOf('Footer', module)
     .addDecorator(story => (
