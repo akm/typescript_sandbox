c11ea354cf07d9354c4f47608ef42fafa9f1582d
akm <akm2000@gmail.com>
2020-08-17 20:19:30 +0900

    :sparkles: --gfm オプションを指定可能に。

--- jsprimer.net/nodecli/main.js
+++ jsprimer.net/nodecli/main.js
@@ -5,10 +5,15 @@ const fs = require("fs");
 // markedモジュールをmarkedオブジェクトとしてインポートする
 const marked = require("marked");
 
+// gfmオプションを定義する
+program.option("--gfm", "GFMを有効にする");
 // コマンドライン引数をパースする
 program.parse(process.argv);
 // ファイルパスをprogram.args配列から取り出す
 const filePath = program.args[0];
+// オプションのパース結果をオブジェクトとして取得する
+const options = program.opts();
+console.log(options.gfm);
 
 // ファイルを非同期で読み込む
 fs.readFile(filePath, { encoding: "utf8" }, (err, file) => {
