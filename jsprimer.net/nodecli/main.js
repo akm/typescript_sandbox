a3bc2257d2009cd4b7c8d0f788ee0b265ce742e9
akm <akm2000@gmail.com>
2020-08-17 20:01:35 +0900

    :sparkles: エンコードを指定しないでファイルを読み込み
    $ node main.js sample.md
    <Buffer 23 20 73 61 6d 70 6c 65 0a>

--- jsprimer.net/nodecli/main.js
+++ jsprimer.net/nodecli/main.js
@@ -1,8 +1,14 @@
 // commanderモジュールをprogramオブジェクトとしてインポートする
 const program = require("commander");
+// fsモジュールをfsオブジェクトとしてインポートする
+const fs = require("fs");
+
 // コマンドライン引数をパースする
 program.parse(process.argv);
-
 // ファイルパスをprogram.args配列から取り出す
 const filePath = program.args[0];
-console.log(filePath);
+
+// ファイルを非同期で読み込む
+fs.readFile(filePath, (err, file) => {
+    console.log(file);
+});
