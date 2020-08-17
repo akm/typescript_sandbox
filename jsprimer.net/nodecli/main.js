ffe81471e2ed8964e9fc2b322576695cd5751e43
akm <akm2000@gmail.com>
2020-08-17 19:57:19 +0900

    :sparkles: commandarモジュールで引数を解釈する処理を追加

--- jsprimer.net/nodecli/main.js
+++ jsprimer.net/nodecli/main.js
@@ -1,1 +1,8 @@
-console.log(process.argv);
+// commanderモジュールをprogramオブジェクトとしてインポートする
+const program = require("commander");
+// コマンドライン引数をパースする
+program.parse(process.argv);
+
+// ファイルパスをprogram.args配列から取り出す
+const filePath = program.args[0];
+console.log(filePath);
