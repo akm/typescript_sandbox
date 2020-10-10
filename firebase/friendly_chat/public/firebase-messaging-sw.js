efb8fb29db609d6041202ff84249a35ea97d3635
akm <akm2000@gmail.com>
2020-10-10 11:47:19 +0900

    :sparkles: Firebaseコンソールでウェブアプリを登録
    登録されていなかったので登録したところ [Firebase SDKの追加] で以下のメッ
    セージが表示されたのでバージョンを合わせてみました。
    
    「これらのスクリプトをコピーして <body> タグの下部に貼り付けます。この作業は Firebase サービスを使用する前に行ってください。
    
    <!-- The core Firebase JS SDK is always required and must be listed first -->
    <script src="/__/firebase/7.23.0/firebase-app.js"></script>
    
    <!-- TODO: Add SDKs for Firebase products that you want to use
         https://firebase.google.com/docs/web/setup#available-libraries -->
    
    <!-- Initialize Firebase -->
    <script src="/__/firebase/init.js"></script>

--- firebase/friendly_chat/public/firebase-messaging-sw.js
+++ firebase/friendly_chat/public/firebase-messaging-sw.js
@@ -1,5 +1,5 @@
-importScripts('/__/firebase/7.22.1/firebase-app.js');
-importScripts('/__/firebase/7.22.1/firebase-messaging.js');
+importScripts('/__/firebase/7.23.0/firebase-app.js');
+importScripts('/__/firebase/7.23.0/firebase-messaging.js');
 importScripts('/__/firebase/init.js');
 
 firebase.messaging();
