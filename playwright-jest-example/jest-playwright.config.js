59b2e7aae252b59754592b8b731be88f5d1d7f9b
akm <akm2000@gmail.com>
2021-02-19 15:36:04 +0900

    :wrench: テスト対象からwebkitを削除

--- playwright-jest-example/jest-playwright.config.js
+++ playwright-jest-example/jest-playwright.config.js
@@ -1,6 +1,7 @@
 // https://github.com/playwright-community/jest-playwright/#configuration
 module.exports = {
-  browsers: ["chromium", "firefox", "webkit"],
+  // テストをそれぞれchromiumとfirefoxで実行する
+  browsers: ["chromium", "firefox"],
   exitOnPageError: false, // GitHub currently throws errors
   launchOptions: {
     headless: true
