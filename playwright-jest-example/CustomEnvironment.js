de1b26f45b8eb3667eced580e6cac337dd45d36b
akm <akm2000@gmail.com>
2021-02-19 18:29:29 +0900

    :sparkles: エラー時にはconsole.logでエラーを出力できるように

--- playwright-jest-example/CustomEnvironment.js
+++ playwright-jest-example/CustomEnvironment.js
@@ -7,16 +7,28 @@ const PlaywrightEnvironment = require('jest-playwright-preset/lib/PlaywrightEnvi
 class CustomEnvironment extends PlaywrightEnvironment {
   async setup() {
     await super.setup()
-    // Your setup
   }
 
   async teardown() {
-    // Your teardown
     await super.teardown()
   }
 
+  testFullPath(test) {
+    return test.parent ? `${this.testFullPath(test.parent)}/${test.name}` : "" // test.name
+  }
+
   async handleTestEvent(event) {
-    console.log(`event.name: ${event.name}`, event)
+    console.log(`event.name: ${event.name}`)
+    // await jestPlaywright.saveCoverage(page)
+
+    if (event.name === 'test_fn_failure') {
+      const t = event.test
+      const fullpath = this.testFullPath(t)
+      for(let i=0; i < t.errors.length; i++) {
+        console.log(`${fullpath} ERROR [${i} / ${t.errors.length}]:`, t.errors[i])
+      }
+    }
+
     // if (event.name === 'test_done' && event.test.errors.length > 0) {
     //   const parentName = event.test.parent.name.replace(/\W/g, '-')
     //   const specName = event.test.name.replace(/\W/g, '-')
@@ -26,5 +38,8 @@ class CustomEnvironment extends PlaywrightEnvironment {
     // }
   }
 }
+
+
+
 
 module.exports = CustomEnvironment
